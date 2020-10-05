3ea298a3fbdd1e6c440e3e3504b767c687f5f800
mothour <kmatiour30@gmail.com>
2020-10-05 07:38:30 -0700

    a

--- BlogPost/settings.py
+++ BlogPost/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
