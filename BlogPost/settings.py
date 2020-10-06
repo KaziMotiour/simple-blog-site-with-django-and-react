ec37e7cb9a3966d0e0ad8a400bb1198bf4ab05ad
mothour <kmatiour30@gmail.com>
2020-10-06 00:02:59 -0700

    authentication permission added

--- BlogPost/settings.py
+++ BlogPost/settings.py
@@ -144,9 +144,11 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+         'rest_framework.authtoken'
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     )
